f3ce0a734642c566337fbf0752b493d30a81df5b
kagunyan25 <mnm.0225kaguya@gmail.com>
2024-09-12 00:48:18 +0900

    ✨ LINEボタンを一旦設置

---
+++ frontend/src/app/sign-in/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { LineButton } from '@/components/LineButton';
+import { useLine } from '@/hooks/useLine';
+
+export const SignIn = () => {
+  const { signIn } = useLine();
+
+  const handleClick = () => {
+    signIn();
+  };
+  return (
+    <>
+      <h1>会員登録ORログイン</h1>
+      <LineButton handleClick={handleClick}></LineButton>
+    </>
+  );
+};
+
+export default SignIn;
---
+++ frontend/src/hooks/useLine.tsx
@@ -0,0 +1,31 @@
+'use client';
+import liff from '@line/liff';
+import { useEffect } from 'react';
+
+export const useLine = () => {
+  useEffect(() => {
+    try {
+      liff.init({ liffId: '2006306110-dVP1PYQQ' });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  const signIn = () => {
+    try {
+      liff.login();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getProfile = async () => {
+    try {
+      const profile = await liff.getProfile();
+      return profile;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return { signIn, getProfile };
+};
